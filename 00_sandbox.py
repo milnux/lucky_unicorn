54b379aabd050d187ce15c71c542b262415e2308
milnux <milner.brook@awatapu.school.nz>
2021-07-06 13:12:15 +1200

    ans var print stat

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -2,5 +2,7 @@
 get_number = int(input("Choose a number? "))
 # Multiply number by 5
 times_five = get_number * 5
+
+answer: str = "{} times five is equal to {}".format(get_number, times_five)
 # Output the results
-print("{} times five is equal to {}".format(get_number, times_five))
+print(answer)
