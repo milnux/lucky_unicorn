d0c05d417bd04ea4ae185e5c0b4229cd01ccacea
Brooklyn Milner <milner.brook@awatapu.school.nz>
2021-07-06 12:53:00 +1200

    Initial commit

---
+++ 00_sandbox.py
@@ -0,0 +1,6 @@
+# Asks users for a number
+get_number = int(input("Choose a number?"))
+# multiply by five
+times_five = get_number * 5
+# output the result
+print("{} times five is equal to {}".format(get_number, times_five))
