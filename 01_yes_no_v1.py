6fac2d1e44be278e677dd298835163fc2c9e4a13
milnux <milner.brook@awatapu.school.nz>
2021-07-09 09:58:49 +1200

    y/n chkr with elif

--- 01_yes_no_v1.py
+++ 01_yes_no_v1.py
@@ -3,6 +3,11 @@ show_instructions = input("Have you ever played this game before").lower()
 # if they say yes, output 'program continues'
 if show_instructions == "yes":
     print("Program continues")
+
 # if they say no, output 'display instructions'
+
+elif show_instructions == "no":
+    print("display instructions")
+# if thy type something other than yes or no 'print error'
 else:
-    print("Display instructions")
+    print("<error> PLease type yes or no")
