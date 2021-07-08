0aac436a0beb79371beac1ae576214d0ad7e01f6
milnux <milner.brook@awatapu.school.nz>
2021-07-09 09:24:27 +1200

    for loop 1st

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,8 +1,4 @@
-# Asks user for a number
-get_number = int(input("Choose a number? "))
-# Multiply number by 5
-times_five = get_number * 5
+# Count up from one to 10
 
-answer = '{} times five is equal to {}'.format(get_number, times_five)
-# Output the results
-print(answer)
+for item in range(1, 10 + 1):
+    print(item)
