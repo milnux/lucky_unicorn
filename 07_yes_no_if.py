5737aaa97ecc22ca7116028815cae1fee9b4df97
milnux <milner.brook@awatapu.school.nz>
2021-07-26 13:19:52 +1200

    y/n ineffiecent method

--- 07_yes_no_if.py
+++ 07_yes_no_if.py
@@ -1,13 +1,20 @@
 # ask the user if they have played before
 show_instructions = input("Have you ever played this game before").strip().lower()
-# if they say yes, output 'program continues'
+
+# if they say yes, output 'program continues
 if show_instructions == "yes":
+    print("program continues")
+
+elif show_instructions == "y":
     print("Program continues")
 
 # if they say no, output 'display instructions'
-
 elif show_instructions == "no":
     print("Display instructions")
-# if they type something other than yes or no 'print error'
+
+elif show_instructions == "n":
+    print("display instrucions")
+
+# if user puts unexpected code, display error message
 else:
-    print("<error> PLease type yes or no")
+    print("Please answer yes / no")
