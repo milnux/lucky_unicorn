131845f07e6363f5f4aa97d7cd705696e6fb2132
milnux <milner.brook@awatapu.school.nz>
2021-08-12 09:24:37 +1200

    fully functional n/ chkr with functions

--- 04_how_much_v1.py
+++ 04_how_much_v1.py
@@ -1,21 +1,21 @@
 # Functions go here...
+def num_check(question, low, high):
+    error = "Please enter an whole number between 1 and 10\n"
 
+    valid = False
+    while not valid:
+        try:
+            # ask the question
+            response = int(input(question))
 
-# Main routine goes here...
+            # if the amount is too low/ too high, give an error message
+            if low < response <= high:
+                return response
 
-error = "Please enter an whole number between 1 and 10\n"
-
-valid = False
-while not valid:
-    try:
-        # ask the question
-        response = int(input("How much would you like to play with? "))
-
-        # if the amount is too low/ too high, give an error message
-        if 0 < response <= 10:
-            print("You have asked to play with ${}".format(response))
-
-        else:
+            # output an error
+            else:
+                print(error)
+        except ValueError:
             print(error)
-    except ValueError:
-        print(error)
+# Main routine goes here...
+how_much = num_check(("How much would you like to play with? ", 0, 10))
