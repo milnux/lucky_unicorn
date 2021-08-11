67645636d1536c799a3d837b59c8d9b829302807
milnux <milner.brook@awatapu.school.nz>
2021-08-12 09:16:18 +1200

    fully functional n/ chkr

--- 04_how_much_v1.py
+++ 04_how_much_v1.py
@@ -3,16 +3,19 @@
 
 # Main routine goes here...
 
-error = "Please enter an whole number between 1 and 10"
+error = "Please enter an whole number between 1 and 10\n"
 
 valid = False
 while not valid:
-    # ask the question
-    response = int(input("How much would you like to play with"))
+    try:
+        # ask the question
+        response = int(input("How much would you like to play with? "))
 
-    # if the amount is too low/ too high, give an error message
-    if 0 < response <= 10:
-        print("You have asked to play with ${}".format(response))
+        # if the amount is too low/ too high, give an error message
+        if 0 < response <= 10:
+            print("You have asked to play with ${}".format(response))
 
-    else:
+        else:
+            print(error)
+    except ValueError:
         print(error)
