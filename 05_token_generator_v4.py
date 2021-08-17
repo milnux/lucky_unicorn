3c64dfd3f83266361e1d8b63e33949f2373f8c8a
milnux <milner.brook@awatapu.school.nz>
2021-08-17 13:27:25 +1200

    token generator, getting a more effecient way

--- 05_token_generator_v4.py
+++ 05_token_generator_v4.py
@@ -1,20 +1,23 @@
 import random
 
 # main routine goes here
-tokens = ["unicorn", "horse", "horse", "horse", "zebra", "zebra", "zebra", "donkey" "donkey" "donkey"]
+
 STARTING_BALANCE = 100
 
 balance = STARTING_BALANCE
 # testing loop to generate 20 tokens
 for item in range(0, 500):
-    chosen = random.choice(tokens)
+    chosen_num = random.randint(1, 100)
 
     # adjust balance
-    if chosen == "unicorn":
+    if 1 <= chosen_num <= 5:
+        chosen == "unicorn"
         balance += 4
-    elif chosen == "donkey":
+    elif 6 >= chosen_num <= 36:
+        chosen == "donkey"
         balance -= 1
     else:
+        chosen = "horse / zebra"
         balance -= 0.5
 
     # output
