a80cd119470423419479b05cf55d74f92db50035
milnux <milner.brook@awatapu.school.nz>
2021-09-10 10:56:59 +1200

    token generator finished, rounds and statements started

--- 05_token_generator_v4.py
+++ 05_token_generator_v4.py
@@ -5,31 +5,38 @@ import random
 STARTING_BALANCE = 100
 
 balance = STARTING_BALANCE
-# testing loop to generate 20 tokens
+# Testing loop to generate 20 tokens
 for item in range(0, 100):
     chosen_num = random.randint(1, 100)
 
-    # adjust balance
+    # Adjust balance
+    # If th erandom # is between 1 and 5
+    # user gets a unicorn(+$4 to balance)
     if 1 <= chosen_num <= 5:
         chosen = "unicorn"
         balance += 4
+
+    # If the random # is between 6 and 36
+    # user gets a donkey (-$1 from balance)
     elif 6 >= chosen_num <= 40:
         chosen = "donkey"
         balance -= 1
-    elif 41 >= chosen_num <= 70:
-        chosen = "horse"
-        balance -= 0.5
+
+    # The token is either a horse or zebra...
+    # in both cases, -$0.50 from the balance
     else:
+        # if the number is even, set the chosen item to a horse
         if chosen_num % 2 == 0:
             chosen = "horse"
+        # otherwise set it to a zebra
         else:
             chosen = "zebra"
         balance -= 0.5
 
-# output
+# Output
 
     print("You got a {}. Your balance is ${:.2f}".format(chosen, balance))
     print()
-    print()
     print("Starting Balance: ${:.2f}".format(STARTING_BALANCE))
-    print("Final Balance: ${:.2f}".format())
+    print("Final Balance: ${:.2f}".format(balance))
+
---
+++ 06_rounds_v01.py
@@ -0,0 +1,11 @@
+# set balance fo testing purposes
+balance = 5
+
+rounds_played = 0
+
+play_again = input("Press <Enter> to play...")
+while play_again == "":
+
+    # increase # of rounds played
+    rounds_played += 11
+
