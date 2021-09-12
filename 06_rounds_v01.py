3f463d9b0ab0d943c7af58c308ad8790f2bffe40
milnux <milner.brook@awatapu.school.nz>
2021-09-13 11:59:24 +1200

    round number implemented, more aesthetic

--- 06_rounds_v01.py
+++ 06_rounds_v01.py
@@ -3,19 +3,25 @@ balance = 5
 
 rounds_played = 0
 
-play_again = input("Press <Enter> to play...")
+play_again = input("Press <Enter> to play...").lower()
 while play_again == "":
 
     # increase # of rounds played
     rounds_played += 1
 
     # print round number
-    print(rounds_played)
+    print()
+    print("*** Round #{} ***".format(rounds_played))
     balance -= 1
     print("balance: ", balance)
     print()
 
-    play_again = input("Press Enter to play again, or 'xxx' to quit")
+    if balance < 1:
+        play_again = "xxx"
+        print("Sorry you have run out of money")
+    else:
+
+        play_again = input("Press Enter to play again, or press 'xxx' to quit")
 
 print()
 print("Final Balance", balance)
