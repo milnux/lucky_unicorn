1f2bf899c13f7dfd3ff7e6fd9d251a6b70933ac9
milnux <milner.brook@awatapu.school.nz>
2021-09-14 13:01:19 +1200

    starting statement decorator

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,5 +1,25 @@
-import random
+greeting = "Welcome to the Lucky Unicorn Game"
+sides = "*" * 3
 
-for item in range(0, 20):
-    number = random.randint(1, 4)
-    print(number, end="\t")
+greeting = "{} {} {}".format(sides, greeting, sides)
+
+top_bottom = "*" * len(greeting)
+
+print()
+print(top_bottom)
+print(greeting)
+print(top_bottom)
+print()
+
+greeting_2 = "Congratulations you got a Unicorn"
+sides = "!" * 3
+
+greeting_2 = "{} {} {}".format(sides, greeting_2, sides)
+
+top_bottom_2 = "!" * len(greeting_2)
+
+print()
+print(top_bottom_2)
+print(greeting_2)
+print(top_bottom_2)
+print()
